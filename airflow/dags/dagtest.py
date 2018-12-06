69cc099977bb8deeb198c00a43dbaecfc11f24ae
aitor <aitormarco1990@gmail.com>
2018-12-06 09:43:31 +0100

    add check session status

--- airflow/dags/dagtest.py
+++ airflow/dags/dagtest.py
@@ -1,11 +1,15 @@
 
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.sensors.http_sensor import HttpSensor
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators import PythonOperator
 from datetime import datetime, timedelta
 import json, pprint, requests, textwrap
+import logging
+from airflow.hooks.http_hook import HttpHook
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
 
 default_args = {
     'owner': 'airflow',
@@ -14,7 +18,7 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5)
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
@@ -22,29 +26,74 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-"""def task_spark()
-    r = requests.get(session_url, headers={'Content-Type': 'application/json'})
-    statements_url = session_url + '/statements'
-    data = {'code': '1 + 1'}
-    r = requests.post(statements_url, data=json.dumps(data), headers=headers)
-    return r.json()"""
-
-"""def init_session(ds, **kwargs):
-    host = 'http://localhost:8998'
-    data = {'kind': 'spark'}
-    headers = {'Content-Type': 'application/json'}
-    r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
-    session_url = host + r.headers['location']"""
 
 
 dag = DAG('etl-spark', default_args=default_args, schedule_interval="@once")
-run_this = SimpleHttpOperator(
+
+
+class SparkLivykHook(BaseOperator):
+    template_fields = ('endpoint', 'data',)
+    template_ext = ()
+    ui_color = '#f4a460'
+
+    @apply_defaults
+    def __init__(self,
+                 endpoint,
+                 method='POST',
+                 data=None,
+                 headers=None,
+                 response_check=None,
+                 extra_options=None,
+                 xcom_push=False,
+                 http_conn_id='http_default',
+                 log_response=False,
+                 *args, **kwargs):
+        super(SparkLivykHook, self).__init__(*args, **kwargs)
+        self.http_conn_id = http_conn_id
+        self.method = method
+        self.endpoint = endpoint
+        self.headers = headers or {}
+        self.data = data or {}
+        self.response_check = response_check
+        self.extra_options = extra_options or {}
+        self.xcom_push_flag = xcom_push
+        self.log_response = log_response
+
+    def execute(self, context):
+        http = HttpHook(self.method, http_conn_id=self.http_conn_id)
+
+        self.log.info("Calling HTTP method")
+
+        response = http.run(self.endpoint,
+                            self.data,
+                            self.headers,
+                            self.extra_options)
+        if self.response_check:
+            if not self.response_check(response):
+                raise AirflowException("Response check returned False.")
+
+        return str(json.loads(response.text)['id'])
+
+
+spark_session = SparkLivykHook(
     http_conn_id='spark',
     task_id='start-session',
     data = json.dumps({'kind': 'spark'}),
     headers = {'Content-Type': 'application/json'},
     endpoint = 'sessions',
     log_response = True,
-    xcom_push = True,
     dag=dag,
 )
+
+
+sensor = HttpSensor(
+    task_id='wait_spark_ready',
+    http_conn_id='spark',
+    endpoint = "{{'/sessions/'+ti.xcom_pull(task_ids='start-session')+'/state'}}",
+    request_params={},
+    response_check=lambda response : True,
+    poke_interval=5,
+    dag=dag,
+)
+
+spark_session >> sensor
